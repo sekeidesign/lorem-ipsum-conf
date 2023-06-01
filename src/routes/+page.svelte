8b404415e7ed17167c119368a8cc57bdc9df363c
Piergiorgio Gonni <pg@sekei.ca>
2023-05-31 23:12:11 -0400

    Play around with settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,8 +20,11 @@
   $: containerHeight;
   let containerWidth;
   $: containerWidth;
+
   let floorEl;
   let ballEl = [];
+  let leftWallEl;
+  let rightWallEl;
 
   onMount(() => {
     const engine = Engine.create();
@@ -42,15 +45,47 @@
       },
       isStatic: true,
     });
+    const leftWall = DomBodies.block(0, containerHeight / 2, {
+      Dom: {
+        render: render,
+        element: leftWallEl,
+      },
+      isStatic: true,
+    });
+    const rightWall = DomBodies.block(containerWidth, containerHeight / 2, {
+      Dom: {
+        render: render,
+        element: rightWallEl,
+      },
+      isStatic: true,
+    });
     const balls = ballEl.map((ball, i) => {
-      return DomBodies.block(100 * (i + 1), 10, {
+      return DomBodies.block(50 * (i + 2), 20 * Math.random(), {
         Dom: {
           render: render,
           element: ball,
         },
+        mass: 0.001,
+        angularVelocity: 0.01,
+        friction: 1,
       });
     });
-    World.add(world, [floor, ...balls]);
+    console.log(balls);
+    World.add(world, [floor, leftWall, rightWall, ...balls]);
+
+    /** Mouse control **/
+    var mouse = Mouse.create(document.body);
+    var MouseConstraint = DomMouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.1,
+        render: {
+          visible: false,
+        },
+      },
+    });
+
+    World.add(world, MouseConstraint);
   });
 </script>
 
@@ -59,13 +94,32 @@
   bind:this={worldContainer}
   bind:offsetHeight={containerHeight}
   bind:clientWidth={containerWidth}
-  class="w-screen h-screen bg-slate-400"
+  class="w-screen h-screen bg-slate-400 overflow-hidden"
 >
   <div bind:this={floorEl} class="h-8 w-screen bg-red-500 absolute" />
-  {#each [1, 2, 3, 4] as _, i}
+  <div bind:this={leftWallEl} class="h-screen w-8 bg-red-500 absolute" />
+  <div bind:this={rightWallEl} class="h-screen w-8 bg-red-500 absolute" />
+  {#each Array(20) as _, i}
     <div
       bind:this={ballEl[i]}
-      class="h-8 w-8 bg-purple-500 rounded-full absolute"
-    />
+      class="h-8 w-56 bg-purple-500 unselectable absolute flex justify-center items-center"
+    >
+      Hello Lorem Ipsum
+    </div>
   {/each}
 </div>
+
+<style>
+  .unselectable {
+    cursor: grab;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  .unselectable:active {
+    cursor: grabbing;
+  }
+</style>
